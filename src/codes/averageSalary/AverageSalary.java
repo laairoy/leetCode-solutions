bfe44c36db08f9492bcd92f3361d20f96f68d669
laairoy <laairoy@yahoo.com.br>
2022-03-18 11:28:30 -0300

    added Average Salary Excluding Max and Min Salary code.

---
+++ src/codes/averageSalary/AverageSalary.java
@@ -0,0 +1,42 @@
+package codes.averageSalary;
+
+/**
+ * @author Marden Laairoy
+ * link: https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+ */
+public class AverageSalary {
+    public double average(int[] salary) {
+        // Opcao 3 (mais rápido)
+        double max, min, average;
+        average = salary[0];
+        max = salary[0];
+        min = salary[0];
+
+        for(int i = 1; i < salary.length ; i++){
+            if(max < salary[i]) max = salary[i];
+            if(min > salary[i]) min = salary[i];
+
+            average += salary[i];
+        }
+
+        average -= max;
+        average -= min;
+
+        return average/(salary.length -2);
+
+        // Opcao 2
+//        double average = 0;
+//        Arrays.sort(salary);
+//        for(int i = 1 ; i < salary.length - 1 ; i++){
+//            average += salary[i];
+//        }
+//        return average/(salary.length -2);
+
+        // Opcao 1
+//          return Arrays.stream(salary)
+//                  .sorted()
+//                  .limit(salary.length - 1)
+//                  .skip(1)
+//                  .average().getAsDouble();
+    }
+}
---
+++ src/codes/averageSalary/AverageSalaryTest.java
@@ -0,0 +1,7 @@
+package codes.averageSalary;
+
+public class AverageSalaryTest {
+    public static void main(String[] args) {
+        System.out.println(new AverageSalary().average(new int[]{4000, 1000, 2000, 3000}));
+    }
+}
